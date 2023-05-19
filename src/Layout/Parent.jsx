8b546f3aaf4dc011fee218182a5879865b80e4cc
athaimahbub <athaimahbub2016@gmail.com>
2023-05-19 11:04:17 +0600

    Header section added

--- src/Layout/Parent.jsx
+++ src/Layout/Parent.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Header from '../Component/Header/Header';
 
 const Parent = () => {
     return (
         <div>
+            <Header></Header>
             <Outlet></Outlet>
         </div>
     );
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import Parent from "../Layout/Parent";
+import Home from "../Layout/Home";
 
 const router = createBrowserRouter([
     {
       path: "/",
       element: <Parent></Parent>,
+      children:[
+        {
+            path:'/',
+            element:<Home></Home>
+        }
+      ]
     },
   ]);
 

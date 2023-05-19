53748812e262a4e053ad400e2da5b33c415ae3d5
athaimahbub <athaimahbub2016@gmail.com>
2023-05-19 23:53:23 +0600

    Blogs are added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Parent from "../Layout/Parent";
 import Home from "../Layout/Home";
+import Blog from "../Layout/blog";
 
 const router = createBrowserRouter([
     {
@@ -10,6 +11,10 @@ const router = createBrowserRouter([
         {
             path:'/',
             element:<Home></Home>
+        },
+        {
+          path:'/blogs',
+          element:<Blog></Blog>
         }
       ]
     },
